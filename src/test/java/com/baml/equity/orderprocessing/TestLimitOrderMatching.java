c4e0d205c32112aac1fe7e10ca952256058cdec2
georgefungkp <georgefungkp@gmail.com>
2025-04-19 20:09:56 +0800

    add broker id

--- src/test/java/com/baml/equity/orderprocessing/TestLimitOrderMatching.java
+++ src/test/java/com/baml/equity/orderprocessing/TestLimitOrderMatching.java
@@ -1,19 +1,18 @@
 package com.baml.equity.orderprocessing;
 
-import java.math.BigDecimal;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
-
 import equity.orderprocessing.LimitOrderMatching;
 import equity.vo.Order;
 import equity.vo.Trade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 public class TestLimitOrderMatching{
 	protected TreeMap<BigDecimal, PriorityQueue<Order>> bidMap;
@@ -23,47 +22,47 @@ public class TestLimitOrderMatching{
 
     @BeforeEach
 	protected void setUp() {
-		orderCmp = new Comparator<Order>() {
-			/**
-			 * Earlier order has higher priority
-			 */
-			@Override
-			public int compare(Order o1, Order o2) {
-				return (o1.getOrderSubmittedTime().compareTo(o2.getOrderSubmittedTime()));
-			}
-		};
-		bidMap = new TreeMap<BigDecimal, PriorityQueue<Order>>(new Comparator<BigDecimal>() {
-			/**
-			 * Lower bid price has higher priority
-			 */
-			@Override
-			public int compare(BigDecimal o1, BigDecimal o2) {
-				return o1.compareTo(o2);
-			}
-		});
-		askMap = new TreeMap<BigDecimal, PriorityQueue<Order>>(new Comparator<BigDecimal>() {
-			/**
-			 * Higher ask price has higher priority
-			 */
-			@Override
-			public int compare(BigDecimal o1, BigDecimal o2) {
-				return o2.compareTo(o1);
-			}
-		});
+		orderCmp = new Comparator<>() {
+            /**
+             * Earlier order has higher priority
+             */
+            @Override
+            public int compare(Order o1, Order o2) {
+                return (o1.getOrderSubmittedTime().compareTo(o2.getOrderSubmittedTime()));
+            }
+        };
+		bidMap = new TreeMap<>(new Comparator<>() {
+            /**
+             * Lower bid price has higher priority
+             */
+            @Override
+            public int compare(BigDecimal o1, BigDecimal o2) {
+                return o1.compareTo(o2);
+            }
+        });
+		askMap = new TreeMap<>(new Comparator<>() {
+            /**
+             * Higher ask price has higher priority
+             */
+            @Override
+            public int compare(BigDecimal o1, BigDecimal o2) {
+                return o2.compareTo(o1);
+            }
+        });
 
-		Order bidOrder1 = new Order("001", 300);
-		PriorityQueue<Order> orderList = new PriorityQueue<Order>(orderCmp);
+		Order bidOrder1 = new Order("Broker_1", "001", 300);
+		PriorityQueue<Order> orderList = new PriorityQueue<>(orderCmp);
 		orderList.add(bidOrder1);
 		bidMap.put(BigDecimal.valueOf(8.1), orderList);
-		Order askOrder1 = new Order("002", 400);
-		Order askOrder2 = new Order("003", 300);
-		Order askOrder3 = new Order("004", 100);
+		Order askOrder1 = new Order("Broker_2", "002", 400);
+		Order askOrder2 = new Order("Broker_2", "003", 300);
+		Order askOrder3 = new Order("Broker_2", "004", 100);
 
-		orderList = new PriorityQueue<Order>(orderCmp);
+		orderList = new PriorityQueue<>(orderCmp);
 		orderList.add(askOrder1);
 		orderList.add(askOrder2);
 		askMap.put(BigDecimal.valueOf(8.5), orderList);
-		orderList = new PriorityQueue<Order>(orderCmp);
+		orderList = new PriorityQueue<>(orderCmp);
 		orderList.add(askOrder3);
 		askMap.put(BigDecimal.valueOf(8.2), orderList);
 
@@ -79,22 +78,22 @@ public class TestLimitOrderMatching{
 
     @Test
 	public void testCrossBid() {
-		PriorityQueue<Order> orderList = new PriorityQueue<Order>(orderCmp);
-		Order bidOrder2 = new Order("005", 200);
+		PriorityQueue<Order> orderList = new PriorityQueue<>(orderCmp);
+		Order bidOrder2 = new Order("Broker_1", "005", 200);
 		orderList.add(bidOrder2);
 		bidMap.put(BigDecimal.valueOf(8.5), orderList);
 		List<Trade> trades = LimitOrderMatching.orderMatching("00001", bidMap, askMap);
 		assertEquals(2, trades.size());
-		Trade trade1 = trades.get(0);
+		Trade trade1 = trades.getFirst();
 		assertEquals("00001", trade1.getStockNo());
-		assertEquals("005", trade1.getBuyBrokerID());
-		assertEquals("004", trade1.getSellBrokerID());
+		assertEquals("Broker_1", trade1.getBuyBrokerID());
+		assertEquals("Broker_2", trade1.getSellBrokerID());
 		assertEquals(100, trade1.getQuantity());
 		assertEquals(8.2, trade1.getExecutedPrice().doubleValue());
 		Trade trade2 = trades.get(1);
 		assertEquals("00001", trade2.getStockNo());
-		assertEquals("005", trade2.getBuyBrokerID());
-		assertEquals("002", trade2.getSellBrokerID());
+		assertEquals("Broker_1", trade2.getBuyBrokerID());
+		assertEquals("Broker_2", trade2.getSellBrokerID());
 		assertEquals(100, trade2.getQuantity());
 		assertEquals(8.5, trade2.getExecutedPrice().doubleValue());	
 		assertEquals(8.1, bidMap.firstKey().doubleValue());
@@ -120,8 +119,8 @@ public class TestLimitOrderMatching{
 
 	@Test
 	public void testBigBid() {
-		PriorityQueue<Order> orderList = new PriorityQueue<Order>(orderCmp);
-		Order bidOrder = new Order("005", 20000);
+		PriorityQueue<Order> orderList = new PriorityQueue<>(orderCmp);
+		Order bidOrder = new Order("Broker_1", "005", 20000);
 		orderList.add(bidOrder);
 		bidMap.put(BigDecimal.valueOf(10), orderList);
 		List<Trade> trades = LimitOrderMatching.orderMatching("00001", bidMap, askMap);
@@ -132,8 +131,8 @@ public class TestLimitOrderMatching{
 
 	@Test
 	public void testBigAsk() {
-		PriorityQueue<Order> orderList = new PriorityQueue<Order>(orderCmp);
-		Order askOrder = new Order("005", 20000);
+		PriorityQueue<Order> orderList = new PriorityQueue<>(orderCmp);
+		Order askOrder = new Order("Broker_1", "005", 20000);
 		orderList.add(askOrder);
 		askMap.put(BigDecimal.valueOf(7), orderList);
 		List<Trade> trades = LimitOrderMatching.orderMatching("00001", bidMap, askMap);
@@ -144,8 +143,8 @@ public class TestLimitOrderMatching{
 
 	@Test
 	public void testJustMatch() {
-		PriorityQueue<Order> orderList = new PriorityQueue<Order>(orderCmp);
-		Order bidOrder = new Order("005", 100);
+		PriorityQueue<Order> orderList = new PriorityQueue<>(orderCmp);
+		Order bidOrder = new Order("Broker_1", "005", 100);
 		orderList.add(bidOrder);
 		bidMap.put(BigDecimal.valueOf(8.2), orderList);
 		List<Trade> trades = LimitOrderMatching.orderMatching("00001", bidMap, askMap);
