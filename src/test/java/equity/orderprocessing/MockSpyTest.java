c46fb41ac5b7729cc86b6b49187e16508b1affbb
georgefungkp <georgefungkp@gmail.com>
2025-06-14 01:36:26 +0800

    clean code

--- src/test/java/equity/orderprocessing/MockSpyTest.java
+++ src/test/java/equity/orderprocessing/MockSpyTest.java
@@ -9,12 +9,12 @@ import equity.objectpooling.OrderBook;
 import equity.objectpooling.Trade;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import util.FileChannelService;
 
@@ -27,108 +27,163 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("Mockito Spy and Mock Behavior Tests")
 public class MockSpyTest {
-    private static final ConcurrentHashMap<String, Order> orderObjMapper = new ConcurrentHashMap<>();
+    
+    // Constants
+    private static final String STOCK_1 = "00001";
+    private static final String BROKER_1 = "Broker 1";
+    private static final String BROKER_2 = "Broker 2";
+    private static final String CLIENT_ORDER_1 = "001";
+    private static final String CLIENT_ORDER_2 = "002";
+    private static final double ORDER_PRICE = 8.1;
+    private static final int ORDER_QUANTITY = 300;
+    private static final int EXPECTED_WRITE_RESULT = 100;
+    private static final int NO_OF_STOCKS = 2;
+    
     private static final Logger log = LogManager.getLogger(MockSpyTest.class);
+    
+    // Test data structures
+    private static final ConcurrentHashMap<String, Order> orderObjMapper = new ConcurrentHashMap<>();
     private final LinkedBlockingQueue<Order> orderQueue = new LinkedBlockingQueue<>();
     private final LinkedBlockingQueue<MarketData> marketDataQueue = new LinkedBlockingQueue<>();
     private final LinkedBlockingQueue<Trade> tradeDataQueue = new LinkedBlockingQueue<>();
-    OrderProcessingJob orderProcessingJob;
-    LimitOrderMatchingJob orderMatching;
-    int noOfStocks = 2;
-    Map<String, OrderBook> orderBooks = new HashMap<>();
+    
+    // Test subjects
+    private OrderProcessingJob orderProcessingJob;
+    private LimitOrderMatchingJob orderMatching;
+    private Map<String, OrderBook> orderBooks;
+    
     @Mock
     private FIXTradeServerApp fixTradeServerApp;
 
     @BeforeEach
-    protected void setUp() {
-//        resultingTradeJob = Mockito.spy(new ResultingTradeJob(tradeDataQueue,fixTradeServerApp,fileChannelService));
-        for (int i = 1; i <= noOfStocks; i++) {
-            orderBooks.put(String.format("%05d", i), new OrderBook(String.format("%05d", i), "Stock " + 1));
-        }
-        orderProcessingJob = new OrderProcessingJob(orderQueue, orderBooks, orderObjMapper);
-
-        OrderBook orderBook = orderBooks.get("00001");
-        orderMatching = new LimitOrderMatchingJob(orderBook, orderObjMapper, marketDataQueue, tradeDataQueue);
-        Order bidOrder1 = RandomOrderRequestGenerator.getNewLimitOrder("00001", "Broker 1",
-                "001", "B", 8.1, 300);
-        orderProcessingJob.putOrder(bidOrder1);
-
-        Order askOrder1 = RandomOrderRequestGenerator.getNewLimitOrder("00001", "Broker 2",
-                "002", "S", 8.1, 300);
-        orderProcessingJob.putOrder(askOrder1);
+    void setUp() {
+        initializeOrderBooks();
+        initializeTestSubjects();
+        setupInitialOrders();
     }
 
-	/**
-	 * Test method for verifying the behavior of the Mockito spy doReturn functionality.
-	 * <p>
-	 * This test method ensures that the specified return value is returned by the mocked method when it is invoked.
-	 * It sets up the Mockito spy for the FileChannelService and mocks the behavior of the writeTradeToFile method to
-	 * @throws IOException if an I/O error occurs
-	 * @throws InterruptedException if a thread is interrupted while waiting
-	 **/
-    @Test
-    public void testSpyDoReturn() throws IOException, InterruptedException {
-		FileChannelService fileChannelService = Mockito.spy(new FileChannelService());
-        // Inject the spy into the resultingTradeJob
-        ResultingTradeJob resultingTradeJob = new ResultingTradeJob(tradeDataQueue,fixTradeServerApp,fileChannelService);
+    @AfterEach
+    void tearDown() {
+        orderObjMapper.clear();
+        marketDataQueue.clear();
+        tradeDataQueue.clear();
+    }
 
-        doReturn(100).when(fileChannelService).writeTradeToFile(any(Trade.class), any(Path.class));
+    private void initializeOrderBooks() {
+        orderBooks = new HashMap<>();
+        for (int i = 1; i <= NO_OF_STOCKS; i++) {
+            String stockId = String.format("%05d", i);
+            orderBooks.put(stockId, new OrderBook(stockId, "Stock " + i));
+        }
+    }
+
+    private void initializeTestSubjects() {
+        orderProcessingJob = new OrderProcessingJob(orderQueue, orderBooks, orderObjMapper);
+        OrderBook orderBook = orderBooks.get(STOCK_1);
+        orderMatching = new LimitOrderMatchingJob(orderBook, orderObjMapper, marketDataQueue, tradeDataQueue);
+    }
+
+    private void setupInitialOrders() {
+        Order bidOrder = createBidOrder();
+        Order askOrder = createAskOrder();
+        
+        orderProcessingJob.putOrder(bidOrder);
+        orderProcessingJob.putOrder(askOrder);
+    }
+
+    private Order createBidOrder() {
+        return RandomOrderRequestGenerator.getNewLimitOrder(
+                STOCK_1, BROKER_1, CLIENT_ORDER_1, "B", ORDER_PRICE, ORDER_QUANTITY);
+    }
+
+    private Order createAskOrder() {
+        return RandomOrderRequestGenerator.getNewLimitOrder(
+                STOCK_1, BROKER_2, CLIENT_ORDER_2, "S", ORDER_PRICE, ORDER_QUANTITY);
+    }
+
+    @Test
+    @DisplayName("Should demonstrate spy doReturn behavior without calling real method")
+    void testSpyDoReturn() throws IOException, InterruptedException {
+        // Given - spy with stubbed behavior
+        FileChannelService fileChannelService = spy(new FileChannelService());
+        ResultingTradeJob resultingTradeJob = new ResultingTradeJob(
+                tradeDataQueue, fixTradeServerApp, fileChannelService);
+        
+        doReturn(EXPECTED_WRITE_RESULT)
+                .when(fileChannelService)
+                .writeTradeToFile(any(Trade.class), any(Path.class));
+        
+        // Initially no interactions
         verify(fileChannelService, never()).writeTradeToFile(any(), any());
 
-        orderMatching.matchTopOrder();
-        Trade testTrade = tradeDataQueue.take();
-        resultingTradeJob.processTradeData(testTrade);
-        verify(fileChannelService, times(1)).writeTradeToFile(any(Trade.class), any(Path.class));
+        // When - execute trade processing
+        Trade trade = executeTradeMatching();
+        resultingTradeJob.processTradeData(trade);
+        
+        // Then - verify method was called exactly once
+        verify(fileChannelService, times(1))
+                .writeTradeToFile(any(Trade.class), any(Path.class));
     }
 
-	/**
-	 * Test method for verifying the behavior of a specific scenario using Mockito spy and when/thenReturn functionalities.
-	 * <p>
-	 * This test case validates the spy mechanism on the FileChannelService class by mocking the behavior of the
-	 * writeTradeToFile method to return a specific value when invoked. It also verifies writeTradeToFile is called in stubbing.
-	 *
-	 * @throws IOException if an I/O error occurs
-	 * @throws InterruptedException if a thread is interrupted while waiting
-	 */
     @Test
-    public void testSpyWhen() throws IOException, InterruptedException {
-		FileChannelService fileChannelService = Mockito.spy(new FileChannelService());
-        // Inject the spy into the resultingTradeJob
-        ResultingTradeJob resultingTradeJob = new ResultingTradeJob(tradeDataQueue,fixTradeServerApp,fileChannelService);
+    @DisplayName("Should demonstrate spy when/thenReturn calling real method during stubbing")
+    void testSpyWhen() throws IOException, InterruptedException {
+        // Given - spy that will call real method during stubbing
+        FileChannelService fileChannelService = spy(new FileChannelService());
+        ResultingTradeJob resultingTradeJob = new ResultingTradeJob(
+                tradeDataQueue, fixTradeServerApp, fileChannelService);
+        
+        // When - stubbing with when() calls the real method with null arguments
         try {
-            assertThrows(NullPointerException.class, (Executable) when(fileChannelService.writeTradeToFile(any(Trade.class), any(Path.class))).thenReturn(100));
-        } catch (Exception _) {
+            assertThrows(NullPointerException.class, () -> {
+                when(fileChannelService.writeTradeToFile(any(Trade.class), any(Path.class)))
+                        .thenReturn(EXPECTED_WRITE_RESULT);
+            });
+        } catch (Exception ignored) {
+            // Expected behavior - spy calls real method during stubbing
         }
-		// When the method writeTradeToFile is called, null values are passed.
+
+        // Then - verify real method was called with null during stubbing
         verify(fileChannelService).writeTradeToFile(isNull(), isNull());
-        orderMatching.matchTopOrder();
-        Trade testTrade = tradeDataQueue.take();
-        resultingTradeJob.processTradeData(testTrade);
-        verify(fileChannelService, times(1)).writeTradeToFile(any(Trade.class), any(Path.class));
+        
+        // When - execute actual trade processing
+        Trade trade = executeTradeMatching();
+        resultingTradeJob.processTradeData(trade);
+        
+        // Then - verify method was called with actual trade
+        verify(fileChannelService, times(1))
+                .writeTradeToFile(any(Trade.class), any(Path.class));
     }
 
-
-    /**
-     * By mocking a class, it won't call method. So, it won't throw exception when calling when().thenReturn()
-     * @throws IOException if an I/O error occurs
-     * @throws InterruptedException if a thread is interrupted while waiting
-     */
     @Test
-    public void testMockWhen() throws IOException, InterruptedException {
-		FileChannelService fileChannelService = Mockito.mock(FileChannelService.class);
-        // Inject the mock into the resultingTradeJob
-        ResultingTradeJob resultingTradeJob = new ResultingTradeJob(tradeDataQueue,fixTradeServerApp,fileChannelService);
+    @DisplayName("Should demonstrate mock when/thenReturn not calling real method")
+    void testMockWhen() throws IOException, InterruptedException {
+        // Given - mock that won't call real method
+        FileChannelService fileChannelService = mock(FileChannelService.class);
+        ResultingTradeJob resultingTradeJob = new ResultingTradeJob(
+                tradeDataQueue, fixTradeServerApp, fileChannelService);
 
-        when(fileChannelService.writeTradeToFile(any(Trade.class), any(Path.class))).thenReturn(100);
-        orderMatching.matchTopOrder();
-        Trade testTrade = tradeDataQueue.take();
-        resultingTradeJob.processTradeData(testTrade);
-        verify(fileChannelService, times(1)).writeTradeToFile(any(Trade.class), any(Path.class));
-
+        // When - stubbing with mock doesn't call real method
+        when(fileChannelService.writeTradeToFile(any(Trade.class), any(Path.class)))
+                .thenReturn(EXPECTED_WRITE_RESULT);
+        
+        // Execute trade processing
+        Trade trade = executeTradeMatching();
+        resultingTradeJob.processTradeData(trade);
+        
+        // Then - verify mocked method was called
+        verify(fileChannelService, times(1))
+                .writeTradeToFile(any(Trade.class), any(Path.class));
     }
 
+    private Trade executeTradeMatching() throws InterruptedException {
+        orderMatching.matchTopOrder();
+        return tradeDataQueue.take();
+    }
 }
